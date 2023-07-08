b843839ed1d57d5043a647bbc1cf0dc6a761c103
rohankumar390 <87806043+rohankumar390@users.noreply.github.com>
2023-07-08 10:58:17 +0530

    Hello boys

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./resume.css";
+import Akash from "./akash.pdf";
+
+const Resume = () => {
+  function cnfm() {
+    window.confirm("Are you sure you want to download Resume");
+  };
+  return (
+    <div className="resume">
+      <div className="resume-left">
+        <div className="resume-left-text">
+          <h1>Resume/CV</h1>
+          <p>
+            "Unveiling Excellence on Paper: A Reflection of Skills and
+            "
+          </p>
+
+          <button className="resume-button" onClick={cnfm}>
+            <a href={Akash} download>
+              Download
+            </a>
+          </button>
+        </div>
+
+      </div>
+      <div className="resume-right">
+        <embed
+          src={Akash}
+          type="application/pdf"
+          width="100%"
+          height="600px"
+        ></embed>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
